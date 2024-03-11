74c1d76369dc6b54a8526eadc82bc96058a25f78
juniorDeveloper8 <be6323787@gmail.com>
2024-03-10 19:11:41 -0500

    subo Proyecto

---
+++ Deber/Reports/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Deber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource3 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.btn_buscar = new System.Windows.Forms.Button();
+            this.btn_report = new System.Windows.Forms.Button();
+            this.txt_buscarDni = new System.Windows.Forms.TextBox();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.dataSetReporting = new Deber.DataSet.DataSetReporting();
+            this.clientDataBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.clientDataTableAdapter = new Deber.DataSet.DataSetReportingTableAdapters.ClientDataTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetReporting)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientDataBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            reportDataSource3.Name = "DataSetClient";
+            reportDataSource3.Value = this.clientDataBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource3);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Deber.ClientReporting.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(12, 64);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(645, 254);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // btn_buscar
+            // 
+            this.btn_buscar.Location = new System.Drawing.Point(344, 26);
+            this.btn_buscar.Name = "btn_buscar";
+            this.btn_buscar.Size = new System.Drawing.Size(75, 23);
+            this.btn_buscar.TabIndex = 1;
+            this.btn_buscar.Text = "Buscar";
+            this.btn_buscar.UseVisualStyleBackColor = true;
+            this.btn_buscar.Click += new System.EventHandler(this.btn_buscar_Click);
+            // 
+            // btn_report
+            // 
+            this.btn_report.Location = new System.Drawing.Point(533, 335);
+            this.btn_report.Name = "btn_report";
+            this.btn_report.Size = new System.Drawing.Size(76, 27);
+            this.btn_report.TabIndex = 2;
+            this.btn_report.Text = "Reporte";
+            this.btn_report.UseVisualStyleBackColor = true;
+            this.btn_report.Click += new System.EventHandler(this.btn_report_Click);
+            // 
+            // txt_buscarDni
+            // 
+            this.txt_buscarDni.Location = new System.Drawing.Point(132, 26);
+            this.txt_buscarDni.Name = "txt_buscarDni";
+            this.txt_buscarDni.Size = new System.Drawing.Size(161, 20);
+            this.txt_buscarDni.TabIndex = 4;
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(43, 340);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(78, 22);
+            this.btn_salir.TabIndex = 5;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // dataSetReporting
+            // 
+            this.dataSetReporting.DataSetName = "DataSetReporting";
+            this.dataSetReporting.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // clientDataBindingSource
+            // 
+            this.clientDataBindingSource.DataMember = "ClientData";
+            this.clientDataBindingSource.DataSource = this.dataSetReporting;
+            // 
+            // clientDataTableAdapter
+            // 
+            this.clientDataTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 374);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.txt_buscarDni);
+            this.Controls.Add(this.btn_report);
+            this.Controls.Add(this.btn_buscar);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataSetReporting)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientDataBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.Button btn_buscar;
+        private System.Windows.Forms.Button btn_report;
+        private System.Windows.Forms.TextBox txt_buscarDni;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.BindingSource clientDataBindingSource;
+        private DataSet.DataSetReporting dataSetReporting;
+        private DataSet.DataSetReportingTableAdapters.ClientDataTableAdapter clientDataTableAdapter;
+    }
+}
+
---
+++ Deber/Reports/Form1.cs
@@ -0,0 +1,87 @@
+﻿using Deber.DataSet;
+using Deber.DataSet.DataSetReportingTableAdapters;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Deber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // para q aparesca el diceño del reporte
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string dni = txt_buscarDni.Text;
+            //this.clientDataTableAdapter.Fill(this.dataSetReporting.ClientData, dni);
+            //this.reportViewer1.RefreshReport();
+        }
+
+        //buscar por dni
+        private void btn_buscar_Click(object sender, EventArgs e)
+        {
+            string dni = txt_buscarDni.Text;
+
+            if (!string.IsNullOrEmpty(dni))
+            {
+                try
+                {
+                    DataSetReporting dataSet = new DataSetReporting();
+
+                    ClientDataTableAdapter clientDataTableAdapter = new ClientDataTableAdapter();
+
+                    clientDataTableAdapter.Fill(dataSet.ClientData, dni);
+
+                    reportViewer1.Reset();
+                    reportViewer1.LocalReport.DataSources.Clear();
+                    reportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WinForms.ReportDataSource("DataSetClient", dataSet.Tables["ClientData"]));
+
+
+                    reportViewer1.LocalReport.ReportPath = @"C:\Users\rober\Escritorio\semaestre\programacion visual\ReportViewer\Deber\ClientReporting.rdlc";
+
+                    reportViewer1.RefreshReport();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al buscar en la base de datos: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingrese un valor para buscar.");
+            }
+
+        }
+        // para exportar el reporte en pdf
+        private void btn_report_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string deviceInfo = "<DeviceInfo><OutputFormat>PDF</OutputFormat></DeviceInfo>";
+                byte[] bytes = reportViewer1.LocalReport.Render("PDF", deviceInfo);
+
+                string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Reporte.pdf");
+                using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                {
+                    fs.Write(bytes, 0, bytes.Length);
+                }
+
+                MessageBox.Show("El informe se ha guardado como PDF en el escritorio.", "Informe Guardado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al generar el informe como PDF: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Cerrar el formulario actual
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
